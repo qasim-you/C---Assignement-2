de96d121534929a380ea8a6c0dbfcd8dc48a4c30
Qasim <qasimyousaf094@gmail.com>
2023-09-22 16:56:13 +0500

    Problems

---
+++ Problems/Problem11.cpp
@@ -0,0 +1,8 @@
+// Program 11: Write a program that inputs miles from the user and converts miles into kilometers. One mile is equal to 1.609km.
+#include <iostream>
+using namespace std ;
+int main(){
+    float miles ,kilometers 
+    cout << "Enter the distance";
+
+}
